3cb2eb95466d5d9b4a8d369517c38a5371c54387
Nathan Kruger <125861818+NathanKru@users.noreply.github.com>
2023-09-27 15:41:02 +0200

    Update index.js

--- Dice game/Dicee Challenge/index.js
+++ Dice game/Dicee Challenge/index.js
@@ -1,5 +1,4 @@
-//Below is die code wat ek self geskryf het deur bietjie te google.
-//PS Die teacher het dit is so moerse langer manier gedoen as die boostne deel van my.
+
 
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 
@@ -20,7 +19,7 @@ else document.querySelector("h1").innerHTML="Its A Draw";
 
 
 
-//Below is what i learnt from you xD and it works if you delete the code above !!!!!! test it !!!!
+//Below is a Turnaru Operator.!!!!!! test it !!!!
 
 
 
